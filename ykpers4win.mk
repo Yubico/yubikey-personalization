42878b5a178cc3850d86ff4bc095388087390a6c
Simon Josefsson <simon@yubico.com>
2010-11-25 14:24:50 +0000

    Add.

---
+++ ykpers4win.mk
@@ -0,0 +1,20 @@
+ykpers4win:
+	rm -rf tmp && mkdir tmp && cd tmp && \
+	wget -q http://yubico-c.googlecode.com/files/libyubikey-1.7.tar.gz && \
+	tar xfa libyubikey-1.7.tar.gz && \
+	cd libyubikey-1.7 && \
+	./configure --host=i686-w64-mingw32 --build=x86_64-unknown-linux-gnu --prefix=$(PWD)/tmp/root && \
+	make install check && \
+	cd .. && \
+	wget -q http://yubikey-personalization.googlecode.com/files/libusb-1.0.8-windows.tar.bz2 && \
+	tar xfa libusb-1.0.8-windows.tar.bz2 && \
+	cd libusb-1.0.8 && \
+	./configure --host=i686-w64-mingw32 --build=x86_64-unknown-linux-gnu --prefix=$(PWD)/tmp/root && \
+	make install && \
+	cd .. && \
+	wget -q http://yubikey-personalization.googlecode.com/files/ykpers-1.3.5.tar.gz && \
+	tar xfa ykpers-1.3.5.tar.gz && \
+	cd ykpers-1.3.5/ && \
+	PKG_CONFIG_PATH=$(PWD)/tmp/root/lib/pkgconfig ./configure --host=i686-w64-mingw32 --build=x86_64-unknown-linux-gnu --prefix=$(PWD)/tmp/root && \
+	make install check && \
+	cd ..
