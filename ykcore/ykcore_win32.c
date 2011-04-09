0ff85dea3dce8eea4ef3edc2272b835e88ef32c6
Simon Josefsson <simon@josefsson.org>
2011-04-09 08:47:47 +0200

    Add native Windows backend.

--- ykcore/ykcore_win32.c
+++
@@ -1,4 +0,0 @@
-/* -*- mode:C; c-file-style: "bsd" -*- */
-/* Please implement this.  Have a look at ykcore_libusb.c or some other
-   already implemented backend for inspiration. */
-#include "ykcore_stub.c"
