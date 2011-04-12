0d6ff680918acc89e20d12c270abe102cc20249f
Simon Josefsson <simon@josefsson.org>
2011-04-12 15:30:24 +0200

    Move all Windows targets to ykpers4win.mk.

--- ykpers4win.mk
+++ ykpers4win.mk
@@ -1,5 +1,9 @@
 LIBYUBIKEYVERSION=1.7
-YKPERSVERSION=1.5.1
+PROJECT=yubikey-personalization
+PACKAGE=ykpers
+VERSION=1.5.1
+USER=simon@yubico.com
+KEYID=2117364A
 
 all: hack-wine ykpers4win32 ykpers4win64
 
@@ -17,18 +21,35 @@ ykpers4win:
 	./configure --host=$(HOST) --build=x86_64-unknown-linux-gnu --prefix=$(PWD)/tmp/root && \
 	make install $(CHECK) && \
 	cd .. && \
-	cp ../ykpers-$(YKPERSVERSION).tar.gz . \
-		|| wget http://yubikey-personalization.googlecode.com/files/ykpers-$(YKPERSVERSION).tar.gz && \
-	tar xfa ykpers-$(YKPERSVERSION).tar.gz && \
-	cd ykpers-$(YKPERSVERSION)/ && \
+	cp ../ykpers-$(VERSION).tar.gz . \
+		|| wget http://yubikey-personalization.googlecode.com/files/ykpers-$(VERSION).tar.gz && \
+	tar xfa ykpers-$(VERSION).tar.gz && \
+	cd ykpers-$(VERSION)/ && \
 	lt_cv_deplibs_check_method=pass_all ./configure --host=$(HOST) --build=x86_64-unknown-linux-gnu --prefix=$(PWD)/tmp/root LDFLAGS=-L$(PWD)/tmp/root/lib CPPFLAGS=-I$(PWD)/tmp/root/include && \
 	make install $(CHECK) && \
 	cd .. && \
 	cd root && \
-	zip -r ../../ykpers-$(YKPERSVERSION)-win$(ARCH).zip *
+	zip -r ../../ykpers-$(VERSION)-win$(ARCH).zip *
 
 ykpers4win32:
 	$(MAKE) -f ykpers4win.mk ykpers4win ARCH=32 HOST=i686-w64-mingw32 CHECK=check
 
 ykpers4win64:
 	$(MAKE) -f ykpers4win.mk ykpers4win ARCH=64 HOST=x86_64-w64-mingw32 CHECK=
+
+upload-ykpers4win:
+	gpg --detach-sign --default-key $(KEYID) \
+		$(PACKAGE)-$(VERSION)-win$(BITS).zip
+	gpg --verify $(PACKAGE)-$(VERSION)-win$(BITS).zip.sig
+	googlecode_upload.py \
+	 -s "OpenPGP signature for $(PACKAGE)-$(VERSION)-win$(BITS).zip." \
+	 -p $(PROJECT) -u $(USER) $(PACKAGE)-$(VERSION)-win$(BITS).zip.sig
+	googlecode_upload.py \
+	 -s "Windows $(BITS)-bit binaries of $(PACKAGE) $(VERSION)" \
+	 -p $(PROJECT) -u $(USER) $(PACKAGE)-$(VERSION)-win$(BITS).zip
+
+upload-ykpers4win32:
+	make upload-ykpers4win BITS=32
+
+upload-ykpers4win64:
+	make upload-ykpers4win BITS=64
