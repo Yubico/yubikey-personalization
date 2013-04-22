5e16a86a7ec0242a8546baaa06d5d5cf2d0b5aed
Klas Lindfors <klas@yubico.com>
2013-04-22 10:43:38 +0200

    have to set CC again for windows builds..

--- build-and-test.sh
+++ build-and-test.sh
@@ -6,6 +6,11 @@ set -x
 autoreconf -i
 
 if [ "x$LIBUSB" = "xwindows" ]; then
+    if [ "x$ARCH" = "x32" ]; then
+        export CC=i686-w64-mingw32-gcc
+    else
+        export CC=x86_64-w64-mingw32-gcc
+    fi
     ./configure --with-backend=stub
     touch ChangeLog
     make dist
