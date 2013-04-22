c457689ebb68bef80ba48e641e5a52f4c53b4afc
Klas Lindfors <klas@yubico.com>
2013-04-22 10:48:08 +0200

    move around, reset CC after make dist

--- build-and-test.sh
+++ build-and-test.sh
@@ -6,14 +6,15 @@ set -x
 autoreconf -i
 
 if [ "x$LIBUSB" = "xwindows" ]; then
+    ./configure --with-backend=stub
+    touch ChangeLog
+    make dist
+
     if [ "x$ARCH" = "x32" ]; then
         export CC=i686-w64-mingw32-gcc
     else
         export CC=x86_64-w64-mingw32-gcc
     fi
-    ./configure --with-backend=stub
-    touch ChangeLog
-    make dist
     make -f ykpers4win.mk ykpers4win${ARCH} `grep ^VERSION Makefile|sed 's/ = /=/'`
 else
     ./configure --with-backend=$LIBUSB
